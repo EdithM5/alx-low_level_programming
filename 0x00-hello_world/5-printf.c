e63dbadba60c6d43766ca386a66ac9be69243a3d
EdithM5 <ewmwenje@gmail.com>
2022-12-08 09:22:02 +0000

    adding the printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+
+
+}
