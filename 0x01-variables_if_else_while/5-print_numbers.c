bd76fa20f26707a887b646cca92f13bf1122249a
EdithM5 <ewmwenje@gmail.com>
2022-12-09 09:53:20 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function prints all numbers in decimal
+ *
+ * Return: always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
