08ec1a7a2456f199d2c674ac2429fc763eb65511
EdithM5 <ewmwenje@gmail.com>
2022-12-09 07:59:40 +0000

    Adding the insomia file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function prints the alphabet in l-case
+ *
+ * Return: always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char i;
+	for (i= 'a'; i<= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+}
