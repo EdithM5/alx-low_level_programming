c576d0e064ec2b801c0785f645c7ae53784cf04c
EdithM5 <ewmwenje@gmail.com>
2022-12-15 17:13:35 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _isupper - checks upercase
+ * @c: what to check
+ * Return: 0 success
+ */
+int_isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
