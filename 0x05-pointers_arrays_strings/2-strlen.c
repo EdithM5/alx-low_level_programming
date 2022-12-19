cbe14863cd874d134aedba963278994924d1843d
EdithM5 <ewmwenje@gmail.com>
2022-12-19 14:04:12 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _strlen - will give the length of a string
+ * @s: deduced by
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
