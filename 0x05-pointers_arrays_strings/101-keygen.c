ce48491cae90771a0cf12c099b68af96b53ca9cf
EdithM5 <ewmwenje@gmail.com>
2022-12-19 21:49:07 +0000

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - generates random paswords
+ * Return: 0 success
+ */
+int main(void)
+{
+	int passw, sum;
+
+	srsnd(time(NULL));
+	sum = 0;
+
+	while (sum <= 2645)
+	{
+		passw = (rand() % 128);
+		sum += passw;
+
+		printf("%c", passw);
+	}
+	printf("%c", 2772 - sum);
+	return (0);
+}
