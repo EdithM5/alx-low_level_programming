a8a44928496cde90b37c6eb69ea8a63ae16ff619
EdithM5 <ewmwenje@gmail.com>
2022-12-19 22:18:51 +0000

    task 11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,21 +7,31 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int j = 1;
-	int k = 1;
+	unsigned int i;
+	unsigned int j = 1;
+	unsigned int k = 0;
+	unsigned int size = 0, m = 1, count = 0;
 
 
 	while (*(s + i) != '\0')
 	{
-		if (*(s + i) < '0' || *(s + i) > 9)
+		if ((size > 0 && *(s + i) < '0') || *(s + i) > '9')
 			break;
 		if (*(s + i) == '-')
 			j *= -1;
 		if ((*(s + i) >= '0') && (*(s + i) <= '9'))
-			k = k + (10 * (*(s + i) - '0'));
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		i++;
 	}
-	i++;
+	for (count = i - size; count < i; count++)
+	{
+		k = k + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
 	return (k * j);
 }
 
