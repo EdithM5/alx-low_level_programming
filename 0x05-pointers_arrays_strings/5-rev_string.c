77510ced8d363cb863dfe3680fc623b312a38507
EdithM5 <ewmwenje@gmail.com>
2022-12-19 15:04:33 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * rev_string - prints string in reverse
+ * @s: the string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int n;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	n = i;
+
+	i = n - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
