9e74d4c46ca6e6485ad5b31aefab5ca28b9e11cf
EdithM5 <ewmwenje@gmail.com>
2022-12-25 18:06:12 +0000

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include<stdio.h>
 /**
  * _strncpy - will concatenate two strings but use n bytes from the second
  * @dest: first string
@@ -8,19 +9,49 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j = 0;
+	int j;
 
-	while  (src[j] != '\0' && j < n)
+	for  (j = 0; src[j] != '\0' && j < n; j++)
 	{
-
 		dest[j] = src[j];
-		j++;
 	}
-	while (j <= n)
+	for (; j < n; j++)
 	{
 		dest[j] = '\0';
-		j++;
 	}
 
 	return (dest);
+}
+
+int main(void)
+{
+       	char s1[98];
+        char *ptr;
+	int i;
+        for (i = 0; i < 98 - 1; i++)
+	{
+		   s1[i] = '*';
+	}
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+	printf("%s\n", s1);
+	printf("%s\n", ptr);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s", s1);
+	printf("%s", ptr);
+	for (i = 0; i < 98; i++)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", s1[i]);
+	}
+	printf("\n");
+	return (0);
 }
