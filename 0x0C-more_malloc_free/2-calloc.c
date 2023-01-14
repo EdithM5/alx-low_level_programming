867564aee32ee7d16c4b39d78432ffa7d28dcf79
EdithM5 <ewmwenje@gmail.com>
2023-01-14 18:03:44 +0000

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,6 +33,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0 || allo == 0)
 		return (NULL);
 
-	_memset(p, 0, (nmemb * size));
+	_memset(allo, 0, (nmemb * size));
+
 	return (allo);
 }
