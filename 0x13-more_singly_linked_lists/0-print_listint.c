b2a5a0812a26da374e03178b08b4efc6d50182ea
EdithM5 <ewmwenje@gmail.com>
2023-02-02 19:13:12 +0000

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "lists.h"
 /**
  * print_listint - prints all the elements of a linked list
  * @h: pointer to the 1st node of the linked list
