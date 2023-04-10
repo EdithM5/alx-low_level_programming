c5bbd10c7853041a45cc2352411ec2c407a8de87
EdithM5 <ewmwenje@gmail.com>
2023-04-10 09:46:46 +0100

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,6 +19,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
